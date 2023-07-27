0cf6cacfcf985707fa12e0c2f6ea37f32853f1c9
anarabishov <anar1991>
2023-07-27 13:04:32 +0400

    margin css

--- src/containers/PageContact/PageContact.tsx
+++ src/containers/PageContact/PageContact.tsx
@@ -38,7 +38,7 @@ const PageContact: FC<PageContactProps> = ({ className = "" }) => {
         <title>Contact || Booking React Template</title>
       </Helmet>
       <div className="mb-24 lg:mb-32">
-        <h2 className="my-16 sm:my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
+        <h2 className="my-16 sm:my-10 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
           Contact
         </h2>
         <div className="container max-w-7xl mx-auto">
--- src/containers/PageLogin/PageLogin.tsx
+++ src/containers/PageLogin/PageLogin.tsx
@@ -69,8 +69,8 @@ const PageLogin: FC<PageLoginProps> = ({ className = "" }) => {
       <Helmet>
         <title>Daxil ol | {appName}</title>
       </Helmet>
-      <div className="container mb-24 lg:mb-32">
-        <h2 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
+      <div className="container mb-10 lg:mb-32">
+        <h2 className="my-10 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
           Daxil ol
         </h2>
         <div className="max-w-md mx-auto space-y-6">
