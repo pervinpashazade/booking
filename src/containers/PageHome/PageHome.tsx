d48bc222b8c97865537db5f7ca8863c313512f8d
anarabishov <anar1991>
2023-08-10 16:46:06 +0400

    css bug

--- src/containers/PageHome/PageHome.tsx
+++ src/containers/PageHome/PageHome.tsx
@@ -121,7 +121,7 @@ function PageHome() {
       {/* GLASSMOPHIN */}
       {/* <BgGlassmorphism /> */}
 
-      <div className="container relative space-y-24 mb-8 md:mb-12 lg:space-y-28 lg:mb-12">
+      <div className="container relative space-y-24 mb-8 md:mb-12 lg:space-y-28 lg:mb-48">
         {/* SECTION HERO */}
         <SectionHero className="pt-0 lg:pt-6" getRoomData={filteredData} />
 
